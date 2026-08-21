eceda38525cd9ffc896676537e322a8ae96a5daf
CH.Jishitha <vtu28055@veltech.edu.in>
2026-08-21 09:56:58 +0530

    Time: 7 ms (97.12%), Space: 46.4 MB (99.45%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,42 +1,25 @@
 class Solution {
-
     public int dayOfYear(String date) {
-
         int year = Integer.parseInt(date.substring(0, 4));
         int month = Integer.parseInt(date.substring(5, 7));
         int day = Integer.parseInt(date.substring(8, 10));
 
-        int[] daysInMonth = {
+        int[] days = {
             31, 28, 31, 30, 31, 30,
             31, 31, 30, 31, 30, 31
         };
 
-        int total = 0;
+        // Leap year
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            days[1] = 29;
+        }
+
+        int result = day;
 
         for (int i = 0; i < month - 1; i++) {
-            total += daysInMonth[i];
+            result += days[i];
         }
 
-        // Add one day to February in a leap year
-        if (month > 2 && isLeapYear(year)) {
-            total++;
-        }
-
-        total += day;
-
-        return total;
-    }
-
-    private boolean isLeapYear(int year) {
-
-        if (year % 400 == 0) {
-            return true;
-        }
-
-        if (year % 100 == 0) {
-            return false;
-        }
-
-        return year % 4 == 0;
+        return result;
     }
 }
